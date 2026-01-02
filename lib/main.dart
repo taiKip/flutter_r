d262e6eabc36b2a454ec09a55f68282fee684e38
tarus <victortarus6@gmail.com>
2026-01-02 14:18:18 +1030

    added character store

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_rpg/screens/home/home.dart';
 import 'package:flutter_rpg/theme.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(MaterialApp(theme: primaryTheme, home: Home()));
+  runApp(
+    ChangeNotifierProvider(
+      child: MaterialApp(theme: primaryTheme, home: Home()),
+    ),
+  );
 }
 
 class Sandbox extends StatelessWidget {
